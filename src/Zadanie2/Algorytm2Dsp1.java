45052cf368dd402c9a64c4d01392e23be8d610bf
Szymon Bartkowiak <szymon.b4310@gmail.com>
2023-10-19 19:01:59 +0200

    zadanie2

---
+++ src/Zadanie2/Algorytm2Dsp1.java
@@ -0,0 +1,29 @@
+package Zadanie2;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+//argorytm scoremana
+public class Algorytm2Dsp1 {
+    public static void main(String[] args) {
+
+        int tab[][] = new int[5][2];
+        tab[0][0] = 4;
+        tab[0][1] = 4;
+        tab[1][0] = 0;
+        tab[1][1] = 0;
+        tab[2][0] = 3;
+        tab[2][1] = 3;
+        tab[3][0] = 3;
+        tab[3][1] = 1;
+        tab[4][0] = 1;
+        tab[4][1] = 1;
+
+
+    }
+}
