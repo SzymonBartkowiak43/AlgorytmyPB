acf59134b31f4e94e762c3fdd732d084d635dbb4
Szymon Bartkowiak <szymon.b4310@gmail.com>
2024-01-24 20:14:37 +0100

    memorise

--- src/BYLECO.java
+++ src/BYLECO.java
@@ -1,31 +1,46 @@
-
-
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 
 public class BYLECO {
-    public static void main(String[] args) {
-//        // Inicjalizacja listy znaków
-//        List<Character> characters = new ArrayList<>();
-//        characters.add('1');
-//        characters.add('2');
-//        characters.add('3');
-//        characters.add('4');
-//        characters.add('5');
-//        characters.add('6');
-//        characters.add('7');
-//        characters.add('8');
-//
-//        // Mieszanie listy w losowej kolejności
-//        Collections.shuffle(characters);
-//
-//        // Wyświetlanie planszy
-//        for (int rzad = 0; rzad < 2; rzad++) {
-//            for (int kolumna = 0; kolumna < 8; kolumna++) {
-//                System.out.print(characters.get(rzad * 8 + kolumna));
-//                System.out.print(characters.get(rzad * 8 + kolumna));
-//                System.out.print(" ");
-//            }
-//            System.out.println(); // Przechodzenie do nowego wiersza po każdym rzędzie
-//        }
-    }
+        public static void main(String[] args) {
+            int[] tab = new int[8];
+            int[][] plansza = new int[2][8];
+            int[][] czyOdkryte = new int[2][8];
+
+            for (int i = 0; i < 2; i++) {
+                Arrays.fill(czyOdkryte[i], 0);
+            }
+
+            Random rand = new Random();
+            int licznik = 0;
+            int rzad = 0;
+            int kolumna = 0;
+            int liczba;
+
+            char[] znak = {'1', '2', '3', '4', '5', '6', '7', '8'};
+
+            while (licznik < 16) {
+                if (rzad == 1 && kolumna == 8) {
+                    break;
+                } else if (kolumna == 8) {
+                    rzad = 1;
+                    kolumna = 0;
+                    System.out.println();
+                }
+
+                liczba = rand.nextInt(8);
+                if (czyOdkryte[rzad][kolumna] != 1) {
+                    System.out.print(znak[liczba] + " ");
+                } else {
+                    System.out.print("X ");
+                }
+                plansza[rzad][kolumna] = liczba;
+                tab[liczba] += 1;
+                kolumna++;
+                licznik++;
+            }
+        }
 }
+
